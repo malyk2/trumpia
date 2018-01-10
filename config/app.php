0d13810e0adcaff37538b8cedd3970e44d14ff93
Taras Kulyk <taras.kulyk1@gmail.com>
2018-01-10 15:07:12 +0200

    add trampia servise provider

--- config/app.php
+++ config/app.php
@@ -177,6 +177,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        Divart\Trumpia\TrumpiaServiceProvider::class,
     ],
 
     /*
